6f441e9f071a8fa53e06091926f356ac511a5bd3
Alexey Romanov <romanalexey@gmail.com>
2017-08-11 00:36:41 +0200

    Use ngFor for cards inside card-list.

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -1,19 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card-list',
   template: `
     <div class="container-fluid text-center pb-5">
-            <div class="row">
-              <app-card class="col-4"></app-card>
-              <app-card class="col-4"></app-card>
-              <app-card class="col-4"></app-card>
-            </div>
-          </div>
+      <div class="row">
+        <app-card *ngFor="let card of cards" [card]="card"></app-card>
+      </div>
+    </div>
   `,
   styles: []
 })
 export class CardListComponent implements OnInit {
+  @Input() cards: Array<any>;
 
   constructor() { }
 
