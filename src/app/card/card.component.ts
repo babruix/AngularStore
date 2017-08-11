3a417e822a706eb8fe1677990b95cdd6ecdd6092
Alexey Romanov <romanalexey@gmail.com>
2017-08-12 00:29:21 +0200

    Use @HostBinding instead of host property.

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
+import {ICard} from '../models/ICard';
 
 @Component({
   selector: 'app-card',
@@ -17,10 +18,10 @@ import {Component, Input, OnInit} from '@angular/core';
   styles: [
     '.card {margin-top: 1.5rem;}'
   ],
-  host: {'class': 'col-2'}
 })
 export class CardComponent implements OnInit {
-  @Input() card: any;
+  @Input() card: ICard;
+  @HostBinding('class') classes = 'col-2';
 
   constructor() {
   }
