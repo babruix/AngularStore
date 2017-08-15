b37e3c8e4ae2c9e38c315b2052c7903b44de734a
Alexey Romanov <romanalexey@gmail.com>
2017-08-16 00:18:56 +0200

    Added custom validator, fixed dependencies

--- src/app/new-card-input/new-card-input.component.ts
+++ src/app/new-card-input/new-card-input.component.ts
@@ -1,6 +1,6 @@
-import {Component, EventEmitter, HostBinding, HostListener, OnInit, Output, ViewChild} from '@angular/core';
-import {ICard} from '../models/ICard';
+import {Component, EventEmitter, HostBinding, HostListener, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms';
+import {dummyValidator} from '../shared/dummy.validator';
 
 @Component({
   selector: 'app-new-card-input',
@@ -15,14 +15,14 @@ import {FormBuilder, FormGroup, NgForm, Validators} from '@angular/forms';
     '.card { margin-bottom: 1.5rem; }',
   ],
 })
-export class NewCardInputComponent implements OnInit {
+export class NewCardInputComponent implements OnInit, OnDestroy {
   @HostBinding('class') class = 'col-8';
   @Output() onCardAdd = new EventEmitter<string>();
 
   @ViewChild('form') public form: NgForm;
 
-  public newCard: ICard = {text: ''};
   newCardForm: FormGroup;
+  private alive = true;
 
   @HostListener('document:keypress', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
@@ -34,12 +34,17 @@ export class NewCardInputComponent implements OnInit {
   constructor(fb: FormBuilder) {
     this.newCardForm = fb.group({
       'text': [null, Validators.compose([
-        Validators.required, Validators.minLength(2)
-      ])],
+          Validators.required, Validators.minLength(2), dummyValidator
+        ]
+      )],
     });
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
   }
 
   addCard(text) {
---
+++ src/app/shared/dummy.validator.ts
@@ -0,0 +1,6 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+export function dummyValidator(control: AbstractControl): ValidationErrors | null {
+  if (!control.value) { return null; }
+  return (control.value.length % 2 === 0) ? null : { dummy: false };
+}
