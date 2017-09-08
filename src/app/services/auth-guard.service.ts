85e33d88193502200195e791ecffc41fc2026094
Alexey Romanov <romanalexey@gmail.com>
2017-09-08 08:43:04 +0200

    Check admin routes access

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,20 +1,35 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(public afAuth: AngularFireAuth, public router: Router) {}
+  constructor(public afAuth: AngularFireAuth
+              , public db: AngularFireDatabase
+              , public router: Router) {}
 
-  canActivate() {
-    console.log('AuthGuard#canActivate called');
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+
     return this.afAuth.authState.map((auth) => {
-        if (!auth) {
+      if (!auth) {
           this.router.navigateByUrl('login');
           return false;
-        }
-        return true;
+      }
+
+      // Check admin routes access
+      const root = this.router.parseUrl(state.url).root;
+      if (root.children.primary.segments.length
+        && root.children.primary.segments[0].path === 'admin') {
+        this.db.object('/users/' + auth.uid).subscribe(u => {
+          if (u.role !== 'admin') {
+            this.router.navigate(['/']);
+          }
+        });
+      }
+
+      return true;
     }).take(1);
   }
 }
